acfa817b68676c63fba0568d7c0a11c44ac8bcba
Karthika Punnengattu Padi Subramanian <76861188+Karthika-ps@users.noreply.github.com>
2026-01-29 21:12:22 +0530

    Delete src directory

--- src/src/data.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-
-DATA_DIR = os.getenv("DATA_DIR", "data")
-DATA_FILE = "creditcard.csv"
-
-def load_data():
-    path = os.path.join(DATA_DIR, DATA_FILE)
-    return pd.read_csv(path)
